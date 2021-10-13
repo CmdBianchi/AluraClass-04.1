ba36a6fa447abc824f41226db294e77ea665c365
CmdBianchi <marcusbianchi92@hotmail.com>
2021-10-13 20:07:05 -0300

    Commit 4.4

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -6,26 +6,42 @@ namespace ByteBank
     {
         static void Main( string[] args )
         {
-            Metodo();
-            Console.ReadLine();
+            try
+            {
+                Metodo();
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine( e.Message );
+                Console.WriteLine( e.StackTrace );
+            }
         }
 
         //Teste com a cadeia de chamada:
         //Metodo -> TestaDivisao -> Dividir
         private static void Metodo()
         {
-            TestaDivisao( 2 );
+            TestaDivisao( 0 );
         }
 
         private static void TestaDivisao( int divisor )
         {
+
             int resultado = Dividir( 10, divisor );
             Console.WriteLine( "Resultado da divisão de 10 por " + divisor + " é " + resultado );
         }
 
         private static int Dividir( int numero, int divisor )
-        {          
-            return numero / divisor;
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch
+            {
+                Console.WriteLine( "Exceção com numero=" + numero + "e divisor=" + divisor );
+                throw;
+            }
         }
     }
 }
